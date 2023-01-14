98bfcc5e1439474433b48138d7969fba714b0c3e
Ali Hassan <alihaxan514@gmail.com>
2023-01-14 17:31:22 +0500

    fixed slider error

--- pages/Component/HomePage.jsx
+++ pages/Component/HomePage.jsx
@@ -3,7 +3,6 @@ import  ClientCandidate  from './ClientCandidate'
 import  CompanyIntro  from './CompanyIntro'
 import  Footer  from './Footer'
 import Hero from './Hero'
-import Slider from '../Slider'
 import  Testimonials  from './Testimonials'
 
 
